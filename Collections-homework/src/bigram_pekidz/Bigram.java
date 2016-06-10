fe4123b2769fb91a161bdd67e4e31dc9399c82a3
Marko Stevankovic <markostevankovic95@gmail.com>
2016-06-10 20:50:25 +0200

    Method which forsees next three charachters based on input string fully implemented.

--- Collections-homework/src/bigram_pekidz/Bigram.java
+++ Collections-homework/src/bigram_pekidz/Bigram.java
@@ -5,6 +5,11 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
+/**
+ * 
+ * @author Marko Stevankovic
+ *
+ */
 public class Bigram 
 {
 	private static String string = "abbcceeeeeeabcc";
@@ -35,8 +40,46 @@ public class Bigram
 		}
 	}
 	
+	public static void forseeNextThreeCharacters(String string, Map map, String forseeFor)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			int maxFrequency = 0;
+			String maxFrequencyBigram = "";
+			
+			Iterator iterator = map.entrySet().iterator();
+			
+			while (iterator.hasNext())
+			{
+				Map.Entry<String, Integer> entry = (Entry<String, Integer>) iterator.next();
+				
+				if (entry.getKey().startsWith(forseeFor.substring(forseeFor.length()-1)))
+				{
+					if (maxFrequency < entry.getValue())
+					{
+						maxFrequency = entry.getValue();
+						maxFrequencyBigram = entry.getKey();
+					}
+				}
+			}
+			
+			if (maxFrequency > 0)
+			{
+				char nextCharacter = maxFrequencyBigram.charAt(1);
+				forseeFor = forseeFor + "" + nextCharacter;
+			}
+		}
+		
+		System.out.print("\nNext three characters(with starting characters): " + forseeFor + "\n");
+		
+	}
+	
 	public static void main(String[] args) 
 	{		
 			printBigram(string, map);
+			
+			String forseeFor = "ja";
+			
+			forseeNextThreeCharacters(string, map, forseeFor);
 	}
 }
